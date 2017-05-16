e92acedd21504c8bbdc0adbe15d993a008b8c657
Heberto Moreno Márquez <nikolasmurdock@gmail.com>
2017-05-16 16:46:45 -0500

    headers and all
    And types and a button for capture. Button still not working. Will work
    later.

--- js/mainPokeTable.js
+++ js/mainPokeTable.js
@@ -7,6 +7,40 @@ var tblBody = document.createElement("tbody");
 
 d3.json("https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json", function(error, pokedex){
 	var pokemon = pokedex.pokemon;
+
+	var headerRow = document.createElement("tr");
+	//set the class of the header row so it can be css styled. 
+	headerRow.setAttribute("class", "headerRow");
+
+	var headNum = document.createElement("th");
+	var headNumText = document.createTextNode("Number");
+	headNum.setAttribute("class", "headerCell");
+	headNum.appendChild(headNumText);
+	headerRow.appendChild(headNum);
+	var headImg = document.createElement("th");
+	var headImgText = document.createTextNode("IMG");
+	headImg.setAttribute("class", "headerCell");
+	headImg.appendChild(headImgText);
+	headerRow.appendChild(headImg);
+	var headName = document.createElement("th");
+	var headNameText = document.createTextNode("Name");
+	headName.setAttribute("class", "headerCell");
+	headName.appendChild(headNameText);
+	headerRow.appendChild(headName);
+	var headType = document.createElement("th");
+	var headTypeText = document.createTextNode("Type");
+	headType.setAttribute("class", "headerCell");
+	headType.appendChild(headTypeText);
+	headerRow.appendChild(headType);
+
+	var headCapt = document.createElement("th");
+	var headCaptText = document.createTextNode("Captured?");
+	headCapt.setAttribute("class", "headerCell");
+	headCapt.appendChild(headCaptText);
+	headerRow.appendChild(headCapt);
+
+	tblHead.appendChild(headerRow);
+
 	for (var i in pokemon){
 		var row = document.createElement("tr");
 		row.setAttribute("class", "dataRow");
@@ -26,6 +60,20 @@ d3.json("https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokede
 		nameCell.setAttribute("class","nameCell");
 		nameCell.appendChild(nameCellText);
 
+		var typeCell= document.createElement("td");
+		var typeCellText= document.createTextNode(pokemon[i].type);
+		typeCell.setAttribute("class","typeCell");
+		typeCell.appendChild(typeCellText);
+
+		var captCell = document.createElement("td");
+		var captCellButton = document.createElement("button");
+		captCellButton.setAttribute("class","button");
+		captCellButton.innerHTML = "Capture";
+		captCellButton.addEventListener("click", function(){
+			alert("Captured!!")
+		});
+		captCell.append(captCellButton);
+
 		/*var cell = document.createElement("td");
 		var cellText = document.createTextNode(specPokemon[i])*/
 		/*console.log(pokemon[i].id);
@@ -33,6 +81,8 @@ d3.json("https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokede
 		row.appendChild(numCell);
 		row.appendChild(imgCell);
 		row.appendChild(nameCell);
+		row.appendChild(typeCell);
+		row.appendChild(captCell);
 
 		tblBody.appendChild(row);
 	}
