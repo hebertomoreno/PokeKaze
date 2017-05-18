73b2e0b6c6a5c097e7b4b0b89c5181e852979f07
Heberto Moreno Márquez <nikolasmurdock@gmail.com>
2017-05-18 14:50:18 -0500

    Some stylings
    And now estoy erizo de andar commiteando todo porque hace rato no
    commitée por un buen tiempo y de repente me dió medio porque no estaba
    funcionando algooo.
    Anyway, tamibbién crear el filter.html

--- js/filterTable.js
+++ js/filterTable.js
@@ -1,3 +1,15 @@
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+var num = getParameterByName('num');
+//var detView = document.getElementById("pokemonDetails");
 var body = document.getElementsByTagName("body")[0];
 
 var tbl = document.createElement("table");
